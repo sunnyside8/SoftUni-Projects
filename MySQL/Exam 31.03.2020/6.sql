d2ac99dfa51fbdb82e42955c2e37bd7f1df331f2
sunnyside8 <88392620+sunnyside8@users.noreply.github.com>
2022-02-10 21:18:45 +0200

    Update 6.sql

--- MySQL/Exam 31.03.2020/6.sql
+++ MySQL/Exam 31.03.2020/6.sql
@@ -3,4 +3,5 @@ p.`date` AS 'date_and_time',
 p.`description`,
 (SELECT COUNT(*) FROM comments WHERE photo_id = p.id) AS'commentsCount'
 FROM photos AS p
-ORDER BY commentsCount DESC,p.id;
+ORDER BY commentsCount DESC,p.id
+LIMIT 5;
